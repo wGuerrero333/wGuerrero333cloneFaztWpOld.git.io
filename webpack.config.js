674f0c2c8d21d822d333f34bcef6c0134e84f17f
wGuerrero333 <wguerreroc1969@gmail.com>
2022-02-17 17:38:18 -0500

    Se agrega html-webpack-plugin ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-    entry: './src/index.js',
-    output :{
-        path: __dirname +'/build',
-        filename: 'bundle.js'
-    }
-}
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template: './index.html'
+  })]
+};
