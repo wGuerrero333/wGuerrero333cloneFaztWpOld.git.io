5a21c0c58160b4424b6575924fbbb38af518f392
wGuerrero333 <wguerreroc1969@gmail.com>
2022-02-18 13:36:44 -0500

    Se agrega mode y port REVISAR entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  // SE AGREGO mode
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js',
   },
-  plugins: [new HtmlWebpackPlugin({
+// puerto
+  devServer:{
+    port:5000
+
+  },
+
+// se agrega el html a la carpeta dist
+  plugins: [
+   
+    new HtmlWebpackPlugin({
       template: './index.html'
   })]
 };
